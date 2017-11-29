6d00bc3e0784eea01ab7d8d0e64ef6191dc1a7b7
Administrator <Administrator@192.168.100.223>
2017-11-29 19:52:51 +0900

    fdasfasdf

---
+++ jsp4/src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test {
+	public static void main(String[] args) {
+//		List<Map<String, String>> list = new ArrayList<Map<String, String>> 
+//	Map<String, String> m = new HashMap <String, String>();
+
+		String name ="ȫ�浿";
+		
+		System.out.println(name);
+	
+	
+	
+	
+	}
+
+}
